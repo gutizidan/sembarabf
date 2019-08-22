63ac0ae195eed7626b90dfb630248c4dea4dbdb3
unknown <gutizidan@gmail.com>
2019-08-22 10:11:24 +0700

    s2

---
+++ app/src/main/java/com/example/tugasXIRPL1/Adapte.java
@@ -0,0 +1,88 @@
+package com.example.tugasXIRPL1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class Adapte extends RecyclerView.Adapter<Adapte.ViewHolder> {
+    private ArrayList<String> arrayList; //Digunakan untuk Judul
+    private ArrayList<Integer> memeList; //Digunakan untuk Image/Gambar
+
+    Adapte(ArrayList<String> arrayList, ArrayList<Integer> memeList){
+        this.arrayList = arrayList;
+        this.memeList = memeList;
+    }
+
+    //ViewHolder Digunakan Untuk Menyimpan Referensi Dari View-View
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView JudulMeme, SubMeme;
+        private ImageView Meme;
+        private RelativeLayout ItemList;
+        private Context context;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            //Untuk Menghubungkan dan Mendapakan Context dari MainActivity
+            context = itemView.getContext();
+
+            //Menginisialisasi View-View untuk kita gunakan pada RecyclerView
+            JudulMeme = itemView.findViewById(R.id.memetitle);
+            SubMeme = itemView.findViewById(R.id.sub_meme);
+            Meme = itemView.findViewById(R.id.meme);
+            ItemList = itemView.findViewById(R.id.item_list);
+            ItemList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent();
+                    switch (getAdapterPosition()){
+                        case 0 :
+                            intent = new Intent(context, PersegiPanjangActivity.class);
+                            break;
+                        case 1 :
+                            intent = new Intent(context, PersegiActivity.class);
+                            break;
+                    }
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Membuat View untuk Menyiapkan dan Memasang Layout yang Akan digunakan pada RecyclerView
+        View V = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_design, parent, false);
+        ViewHolder VH = new ViewHolder(V);
+        return VH;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        //Memasukan Nilai/Value Pada View-View Yang Telah Dibuat
+        final String Nama = arrayList.get(position);//Mengambil data sesuai dengan posisi yang telah ditentukan
+        holder.JudulMeme.setText(Nama);
+        holder.SubMeme.setText("Gambar Meme Ke: "+position);
+        holder.Meme.setImageResource(memeList.get(position)); // Mengambil gambar sesuai posisi yang telah ditentukan
+    }
+
+    @Override
+    public int getItemCount() {
+        //Menghitung Ukuran/Jumlah Data Yang Akan Ditampilkan Pada RecyclerView
+        return arrayList.size();
+    }
+
+}
--- app/src/main/java/com/example/tugasXIRPL1/ThreeFragment.java
+++ app/src/main/java/com/example/tugasXIRPL1/ThreeFragment.java
@@ -1,17 +1,38 @@
 package com.example.tugasXIRPL1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class ThreeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> JudulMeme;
+    private ArrayList<Integer> GambarMeme;
+    //Daftar Judul
+    private String[] Judul = {"KUBUS", "BALOK"};
+    //Daftar Gambar
+    private int[] Gambar = {R.drawable.ic_kubus, R.drawable.ic_balok};
 
-    public ThreeFragment() {
-        // Required empty public constructor
-    }
+//    private void showSelectedHero(bangun hero) {
+//        Toast.makeText(getContext(), "Kamu memilih " + hero.getNama(), Toast.LENGTH_SHORT).show();
+//
+//    }
+
+//    public ThreeFragment() {
+//
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +43,26 @@ public class ThreeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+        JudulMeme = new ArrayList<>();
+        GambarMeme = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.recycler);
+        DaftarItem();
+        //Menggunakan Layout Manager, Dan Membuat List Secara Vertical
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        adapter = new Adapte(JudulMeme, GambarMeme);
+        //Memasang Adapter pada RecyclerView
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+    private void DaftarItem(){
+        for (int w=0; w<Judul.length; w++){
+            GambarMeme.add(Gambar[w]);
+            JudulMeme.add(Judul[w]);
+        }
     }
 
 }
